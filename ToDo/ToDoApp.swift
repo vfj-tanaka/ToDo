4f3f6cae263e025d10e5767e9aa5dc7f34c5597f
vfj-tanaka <108640688+vfj-tanaka@users.noreply.github.com>
2022-10-03 11:54:26 +0900

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by mtanaka on 2022/10/03.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
